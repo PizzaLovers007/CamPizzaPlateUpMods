576c4ac0c5bff2c5712b494c37bdf6571f4e458c
PizzaLovers007 <trex@parkvue.com>
2023-10-02 22:09:37 -0700

    Attempt to update appliance description

---
+++ PlateUpShowUpgrade/Patches/AppendApplianceDescriptionPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using KitchenData;
+
+namespace PlateUpShowUpgrade.Patches
+{
+    [HarmonyPatch]
+    public class AppendApplianceDescriptionPatch
+    {
+        [HarmonyPatch(typeof(ApplianceInfo), "Import")]
+        [HarmonyPostfix]
+        public static void Import(ApplianceInfo __instance) {
+            __instance.Description += "ASDF";
+        }
+    }
+}
