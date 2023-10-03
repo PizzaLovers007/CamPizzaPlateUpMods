39a87b707f4f52fcc329a6986e5c78e64d725bbc
Elizabeth Trinh <e.n.trinh@outlook.com>
2023-10-02 23:06:20 -0700

    Modify appliance ping to show the next upgrades

---
+++ PlateUpShowUpgrade/Patches/NextUpgradeIndicatorPatch.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using HarmonyLib;
+using KitchenData;
+using static KitchenData.Appliance;
+
+namespace PlateUpShowUpgrade.Patches
+{
+    [HarmonyPatch]
+    public class NextUpgradeIndicatorPatch
+    {
+        [HarmonyPatch(typeof(Appliance), "Localise")]
+        [HarmonyPostfix]
+        public static void AddUpgradeInfo(Appliance __instance) {
+            if (!__instance.HasUpgrades) {
+                return;
+            }
+            __instance.Sections.Add(new Section {
+                Title = "Next Upgrade",
+                Description =
+                    string.Join(", ", __instance.Upgrades.Select(app => app.Name).ToList()),
+                RangeDescription = ""
+            });
+        }
+    }
+}
