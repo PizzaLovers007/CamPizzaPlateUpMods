39643287f83995cfdc9c5fc2256cd62bbe78528b
PizzaLovers007 <trex@parkvue.com>
2023-10-08 00:07:12 -0700

    Only enable logging for debug build

--- CamPizza.BetterCustomerPerformance/ModLogger.cs
+++ CamPizza.BetterCustomerPerformance/ModLogger.cs
@@ -1,10 +1,25 @@
-﻿using Kitchen;
+﻿using System.Diagnostics;
+using Kitchen;
 
 namespace CamPizza.BetterCustomerPerformance
 {
     public static class ModLogger
     {
+        private static bool debugging;
+
+        static ModLogger() {
+            CheckForDebugging();
+        }
+
+        [Conditional("DEBUG")]
+        public static void CheckForDebugging() {
+            debugging = true;
+        }
+
         public static void Log(string message) {
+            if (!debugging) {
+                return;
+            }
             Logger.Log(LogSource.Generic, $"[BetterCustomerPerformance] " + message);
         }
     }
