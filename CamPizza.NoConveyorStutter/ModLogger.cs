691675ec0e5ab0b6d4f7ed9f7010693584653481
PizzaLovers007 <trex@parkvue.com>
2023-10-14 01:05:40 -0700

    Create NoConveyorStutter project

---
+++ CamPizza.NoConveyorStutter/ModLogger.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Kitchen;
+
+namespace CamPizza.NoConveyorStutter
+{
+    public static class ModLogger
+    {
+        private static bool debugging;
+
+        static ModLogger() {
+            CheckForDebugging();
+        }
+
+        [Conditional("DEBUG")]
+        public static void CheckForDebugging() {
+            debugging = true;
+        }
+
+        public static void Log(string message) {
+            if (!debugging) {
+                return;
+            }
+            Logger.Log(LogSource.Generic, $"[NoConveyorStutter] " + message);
+        }
+    }
+}
