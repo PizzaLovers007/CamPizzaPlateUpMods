f50f9223ff501ffd306a495b3230fba4dabc5a97
PizzaLovers007 <trex@parkvue.com>
2023-10-14 02:12:43 -0700

    1st attempt at stopping conveyor stutter

---
+++ CamPizza.NoConveyorStutter/Patches/AllowParallelGrabItemsPatch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using Kitchen;
+using UnityEngine;
+
+namespace CamPizza.NoConveyorStutter.Patches
+{
+    [HarmonyPatch]
+    public class AllowParallelGrabItemsPatch
+    {
+        private static readonly HashSet<Vector3> updatingPositionsSet = new HashSet<Vector3>();
+
+        private static bool prevHasPerformedAction;
+
+        [HarmonyPatch("Kitchen.GrabItems+<>c__DisplayClass_OnUpdate_LambdaJob0, KitchenMode", "OriginalLambdaBody")]
+        [HarmonyPrefix]
+        public static void OriginalLambdaBodyPrefix(
+                ref bool ___has_performed_action, CPosition pos) {
+            Vector3 grabPos = CalculateGrabPosition(pos);
+            ___has_performed_action = updatingPositionsSet.Contains(grabPos);
+
+            prevHasPerformedAction = ___has_performed_action;
+        }
+
+        [HarmonyPatch("Kitchen.GrabItems+<>c__DisplayClass_OnUpdate_LambdaJob0, KitchenMode", "OriginalLambdaBody")]
+        [HarmonyPostfix]
+        public static void OriginalLambdaBodyPostfix(
+                bool ___has_performed_action, CPosition pos) {
+            if (___has_performed_action != prevHasPerformedAction) {
+                Vector3 grabPos = CalculateGrabPosition(pos);
+                updatingPositionsSet.Add(grabPos);
+            }
+        }
+
+        private static Vector3 CalculateGrabPosition(CPosition pos) {
+            return (pos.Forward(-1f) + pos).Rounded();
+        }
+    }
+}
---
+++ CamPizza.NoConveyorStutter/Patches/AllowParallelPushItemsPatch.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using Kitchen;
+using Unity.Entities;
+using UnityEngine;
+
+namespace CamPizza.NoConveyorStutter.Patches
+{
+    [HarmonyPatch]
+    public class AllowParallelPushItemsPatch
+    {
+        private static readonly HashSet<Vector3> updatingPositionsSet = new HashSet<Vector3>();
+
+        private static PushItems? pushItemsInstance;
+        private static bool prevHasPerformedAction;
+
+        [HarmonyPatch(typeof(PushItems), "OnUpdate")]
+        [HarmonyPrefix]
+        public static void OnUpdatePrefix(PushItems __instance) {
+            pushItemsInstance = __instance;
+            updatingPositionsSet.Clear();
+        }
+
+        [HarmonyPatch("Kitchen.PushItems+<>c__DisplayClass_OnUpdate_LambdaJob0, KitchenMode", "OriginalLambdaBody")]
+        [HarmonyPrefix]
+        public static void OriginalLambdaBodyPrefix(
+                ref bool ___has_performed_action,
+                Entity e,
+                CPosition pos,
+                CConveyPushItems push) {
+            Vector3 pushPos = CalculatePushingPosition(e, pos, push);
+            ___has_performed_action = updatingPositionsSet.Contains(pushPos);
+
+            prevHasPerformedAction = ___has_performed_action;
+        }
+
+        [HarmonyPatch("Kitchen.PushItems+<>c__DisplayClass_OnUpdate_LambdaJob0, KitchenMode", "OriginalLambdaBody")]
+        [HarmonyPostfix]
+        public static void OriginalLambdaBodyPostfix(
+                bool ___has_performed_action,
+                Entity e,
+                CPosition pos,
+                CConveyPushItems push) {
+            if (___has_performed_action != prevHasPerformedAction) {
+                Vector3 pushPos = CalculatePushingPosition(e, pos, push);
+                updatingPositionsSet.Add(pushPos);
+            }
+        }
+
+        private static Vector3 CalculatePushingPosition(
+                Entity e, CPosition pos, CConveyPushItems push) {
+            if (pushItemsInstance == null) {
+                return Vector3.zero;
+            }
+
+            Orientation orientation = Orientation.Up;
+            if (pushItemsInstance.EntityManager.HasComponent<CConveyPushRotatable>(e)) {
+                var rotateComponent =
+                    pushItemsInstance.EntityManager.GetComponentData<CConveyPushRotatable>(e);
+                if (rotateComponent.Target != Orientation.Null) {
+                    orientation = rotateComponent.Target;
+                }
+            }
+            Vector3 forward =
+                pos.Rotation.RotateOrientation(orientation).ToOffset()
+                * ((!push.Reversed) ? 1 : (-1));
+            return (pos + forward).Rounded();
+        }
+    }
+}
